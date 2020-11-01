01e11f7decf7364b939fb5f844248408023e4b77
Alexander Tsioma <a.tsioma84@gmail.com>
2020-11-01 16:41:03 +0100

    test puppeteer

---
+++ src/puppeteer/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const puppeteerRenderer = require('puppeteer-renderer-middleware');
+app.use(
+	puppeteerRenderer({
+		url: 'http://localhost:5000/renderer',
+	}),
+);
+
+app.use(express.static('@/../dist'));
+
+app.use((req, res) => res.redirect('/'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
